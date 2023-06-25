4117e9f35303742e032e84c8a880a369dbd55e63
sebastianpulak <pulakosso@gmail.com>
2023-06-25 22:58:58 +0200

    service worker update reload on new version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwUpdate, VersionReadyEvent} from "@angular/service-worker";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private swUpdate: SwUpdate) {
+  }
   title = 'words-racer';
+
+  ngOnInit() {
+    this.checkForUpdates();
+  }
+
+  public checkForUpdates(): void {
+    this.swUpdate.versionUpdates.
+    pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+      .subscribe(() => {
+        window.location.reload();
+    });
+  }
 }
