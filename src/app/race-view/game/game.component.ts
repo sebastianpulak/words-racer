3f4c2e8da34bb3c1097d2604291246b81746a39e
sebastianpulak <pulakosso@gmail.com>
2023-06-25 22:05:20 +0200

    implemented countdown to start

--- src/app/race-view/game/game.component.ts
+++ src/app/race-view/game/game.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
@@ -16,14 +16,21 @@ import { generate } from "random-words";
     CommonModule
   ]
 })
+
 export class GameComponent {
   @ViewChild('currentWordInput', {static: false}) currentWordInput!: ElementRef<HTMLInputElement>;
+  @Input() gameInProgress!: boolean;
   currentWordValue = '';
   randomWords: Array<string> = generate(20);
   wordIndex = 0;
   selectedWord = this.randomWords[this.wordIndex];
 
   onWordValueChange(value: string) {
+    if (!this.gameInProgress) {
+      this.currentWordValue = '';
+      this.currentWordInput.nativeElement.value = '';
+      return;
+    }
     if (value === this.selectedWord) {
       this.wordIndex++;
       this.selectedWord = this.randomWords[this.wordIndex];
--- src/app/race-view/race-view.component.ts
+++ src/app/race-view/race-view.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {MatInputModule} from "@angular/material/input";
 import {GameComponent} from "./game/game.component";
+import {map, takeLast, takeWhile, timer} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-race-view',
@@ -8,11 +10,25 @@ import {GameComponent} from "./game/game.component";
   styleUrls: ['./race-view.component.scss'],
   imports: [
     MatInputModule,
-    GameComponent
+    GameComponent,
+    AsyncPipe
   ],
   standalone: true
 })
 export class RaceViewComponent {
-  currentWordValue = '';
+  gameInProgress = false;
+  countdownStarted = false;
+  secondsRemaining$ = timer(0, 1000).pipe(
+    map(n => 5 - n),
+  takeWhile(n => n >= 1)
+);
+
+  startGame() {
+    this.countdownStarted = true;
+    this.secondsRemaining$.pipe(takeLast(1)).subscribe(() => {
+      this.gameInProgress = true;
+      this.countdownStarted = false;
+    })
+  }
 
 }
