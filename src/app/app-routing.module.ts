37c981d1572c84f71013119dd7e19cd33144dc30
sebastianpulak <pulakosso@gmail.com>
2023-06-25 22:35:39 +0200

    fix for same url reload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NoPreloading, RouterModule, Routes} from '@angular/router';
 import {WelcomeComponent} from "./welcome/welcome.component";
 
 const routes: Routes = [
@@ -22,7 +22,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: NoPreloading,
+    onSameUrlNavigation: 'reload'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
