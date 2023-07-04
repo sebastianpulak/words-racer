21fa68693832f81df9f5d4c78553e572f511185d
sebastianpulak <pulakosso@gmail.com>
2023-07-04 18:18:15 +0200

    more rxjs refactor

--- src/app/race-view/game/game.component.ts
+++ src/app/race-view/game/game.component.ts
@@ -44,13 +44,23 @@ export class GameComponent implements OnInit {
   destroyRef = inject(DestroyRef);
 
   ngOnInit() {
-      this.gameState.randomWords$.pipe(
+    this.subscribeToService();
+  }
+
+  private subscribeToService() {
+    this.gameState.randomWords$.pipe(
         takeUntilDestroyed(this.destroyRef)
       ).subscribe((randomWords: Array<string>) => {
-          this._randomWords = randomWords;
-          this.wordIndex = 0;
-          this.selectWord();
-          this.resetState();
+        this._randomWords = randomWords;
+        this.wordIndex = 0;
+        this.selectWord();
+        this.resetState();
+    })
+
+    this.gameState.gameInProgress$.pipe(
+        takeUntilDestroyed(this.destroyRef)
+      ).subscribe((inProgress: boolean) => {
+        this.inProgress = inProgress;
     })
   }
 
@@ -60,20 +70,14 @@ export class GameComponent implements OnInit {
 
   private resetState() {
     this.currentWordValue = '';
-    if (this.currentWordInput) {
-      this.currentWordInput.nativeElement.value = '';
-    }
   }
 
   onWordValueChange(value: string) {
-    this.gameState.gameInProgress$.pipe(
-      takeUntilDestroyed(this.destroyRef)
-    ).subscribe((inProgress: boolean) => {
-        this.inProgress = inProgress;
-    })
-
     if (!this.inProgress) {
       this.resetState();
+      if (this.currentWordInput) {
+        this.currentWordInput.nativeElement.value = '';
+      }
       this.showTooltip.emit();
       return;
     }
